dab2ffd8ac8f8be04b5f168d96fe4b36ee8b4bad
Naheel-Azawy <naheelazawy@gmail.com>
2018-08-22 06:45:07 +0300

    init

---
+++ arabic-reshape.el
@@ -0,0 +1,46 @@
+
+(defun arabic-safe-save ()
+  (remove-hook 'after-save-hook 'arabic-reshape-hook t)
+  (save-buffer)
+  (if arabic-reshape-mode
+      (add-hook 'after-save-hook 'arabic-reshape-hook nil t)))
+
+(defun arabic-reshape-priv ()
+  (let ((loc (point)))
+    (shell-command
+     (concat "cat '" buffer-file-name "' | arabic-reshape")
+     (current-buffer))
+    (delete-trailing-whitespace)
+    (goto-char loc)))
+
+(defun arabic-unshape-priv ()
+  (let ((loc (point)))
+    (shell-command
+     (concat "cat '" buffer-file-name "' | env ARRESHOPT='u' arabic-reshape")
+     (current-buffer))
+    (delete-trailing-whitespace)
+    (goto-char loc)))
+
+(defun arabic-reshape ()
+  (interactive)
+  (arabic-safe-save)
+  (arabic-reshape-priv))
+
+(defun arabic-unshape ()
+  (interactive)
+  (arabic-safe-save)
+  (arabic-unshape-priv))
+
+(defun arabic-reshape-hook ()
+  (arabic-unshape-priv)
+  (arabic-reshape-priv)
+  (arabic-safe-save))
+
+(define-minor-mode arabic-reshape-mode
+  "Shape and unshape Arabic characters"
+  :lighter " AP"
+  (if arabic-reshape-mode
+      (progn
+        (arabic-reshape)
+        (add-hook 'after-save-hook 'arabic-reshape-hook nil t))
+    (remove-hook 'after-save-hook 'arabic-reshape-hook t)))
